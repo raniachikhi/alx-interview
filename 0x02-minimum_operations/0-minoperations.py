e468bf8975d14adde375eadcefcac0fcdd4b61fa
raniachikhi <raniach0653@gmail.com>
2024-03-21 16:28:27 -0700

    add

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-""" Minimum Operations
-    """
-
 
 def minOperations(n: int) -> int:
-    """ Minimum Operations needed to get n H characters."""
+    """ Minimum Operations needed to get n H characters. """
     operations = 0
     factor = 2
     if n < 2:
         return 0
-
     while factor <= n:
         while n % factor == 0:
             operations += factor
             n = n // factor
         factor += 1 
-    
     return operations
