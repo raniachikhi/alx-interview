e6704de7389b43b62359389f549b3f835a6f3e58
raniachikhi <raniach0653@gmail.com>
2024-03-21 16:33:10 -0700

    add

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-
 def minOperations(n: int) -> int:
-    """ Minimum Operations needed to get n H characters. """
-    operations = 0
-    factor = 2
-    if n < 2:
-        return 0
-    while factor <= n:
-        while n % factor == 0:
-            operations += factor
-            n = n // factor
-        factor += 1 
-    return operations
+    op = 0
+    body_length = 1
+    clipboard = 0
+    while body_length < n:
+        if n % body_length == 0:
+            clipboard = body_length
+            op += 1
+        body_length += clipboard
+        op += 1
+    return op
