f279458b5de4406f4869a9e94de8173960c52146
raniachikhi <raniach0653@gmail.com>
2024-03-27 18:13:07 -0700

    add

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,49 +1,35 @@
 #!/usr/bin/python3
-
 import sys
+import re
+import signal
 
+def print_statistics():
+    print("File size: {}".format(file_sizes))
+    for code in sorted(status_codes.keys()):
+        print("{}: {}".format(code, status_codes[code]))
 
-def print_msg(dict_sc, total_file_size):
-    """
-    Print msg
-    """
+def signal_handler(sig, frame):
+    print_statistics()
+    sys.exit(0)
 
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+signal.signal(signal.SIGINT, signal_handler)
 
+file_sizes = 0
+status_codes = {}
+line_count = 0
 
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+line_pattern = re.compile(r'^\S+ - \[\S+ \S+\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)$')
 
-try:
-    for line in sys.stdin:
-        parsed_line = line.split()
-        parsed_line = parsed_line[::-1]
+for line in sys.stdin:
+    match = line_pattern.match(line.strip())
+    if match:
+        status_code, file_size = match.groups()
+        file_sizes += int(file_size)
+        status_codes[status_code] = status_codes.get(status_code, 0) + 1
 
-        if len(parsed_line) > 2:
-            counter += 1
+    line_count += 1
+    if line_count % 10 == 0:
+        print_statistics()
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])
-                code = parsed_line[1]
+print_statistics()
 
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
-
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
-
-finally:
-    print_msg(dict_sc, total_file_size)
